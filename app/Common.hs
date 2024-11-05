3cd9f704802fd9f5cce3d4474ad3ac7ce68c2bac
faycarsons <faycarsons23@gmail.com>
2024-11-05 10:23:02 -0500

    Allow multi-word nicks, switch to strict IntMap for client lookup

--- app/Common.hs
+++ app/Common.hs
@@ -2,6 +2,8 @@ module Common where
 
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
+import GHC.IO.Handle (hFlush)
+import System.IO (stdout)
 
 appHeader :: String
 appHeader =
@@ -23,6 +25,12 @@ appHeader =
     , "                                                "
     ]
 
+clearScreen :: IO ()
+clearScreen =
+  putStr "\ESC[2J"
+    *> putStr "\ESC[H"
+    *> hFlush stdout
+
 data Message
   = Quit
   | SetNick ByteString
@@ -32,12 +40,14 @@ data Message
 parseMessage :: String -> Maybe Message
 parseMessage message =
   if null message
-    then Nothing
+    then Just Quit
     else return $ case words message of
       "/quit" : _ -> Quit
-      "/setNick" : nick : _ -> SetNick $ BS.pack nick
+      "/setNick" : nick -> SetNick $ pack nick
       "/showNick" : _ -> ShowNick
-      msg -> GotMessage $ BS.pack (unwords msg)
+      msg -> GotMessage $ pack msg
+ where
+  pack = BS.unwords . map BS.pack
 
 backlog :: Int
 backlog = 1
--- app/Host.hs
+++ app/Host.hs
@@ -13,8 +13,8 @@ import Control.Monad (forM_)
 import Control.Monad.Fix (fix)
 import Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as BS
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as Map
 import GHC.Base (failIO)
 import Network.Socket
 import System.IO (BufferMode (NoBuffering), Handle, IOMode (ReadWriteMode), hClose, hFlush, hGetLine, hSetBuffering, stdin, stdout)
@@ -26,7 +26,8 @@ data MessageSource
 data Connection
   = Connection
   { conn :: !Handle
-  , clientId :: Int
+  , clientId :: !Int
+  , nick :: Maybe ByteString
   }
 
 data Server
@@ -34,10 +35,7 @@ data Server
   { sock :: !Socket
   , hostNick :: !(TVar ByteString)
   , numClients :: !(TVar Int)
-  , clients :: !(TVar (Map Int Connection))
-  , -- Client metadata is currently just client's nicknames, but in the future it
-    -- could be time active, or a rate-limiting variable
-    clientMetadata :: !(TVar (Map Int (Maybe ByteString)))
+  , clients :: !(TVar (IntMap Connection))
   , -- Messages sent to the mailbox are either a bytestring from the Host's stdin,
     -- or a message from a client
     mailbox :: !(TQueue MessageSource)
@@ -47,10 +45,9 @@ newServer :: Socket -> IO Server
 newServer sock = do
   numClients <- atomically $ newTVar 0
   clients <- atomically $ newTVar Map.empty
-  clientMetadata <- atomically $ newTVar Map.empty
   mailbox <- atomically $ newTQueue
   hostNick <- atomically $ newTVar (BS.pack "Host")
-  return Server{sock, hostNick, numClients, clients, clientMetadata, mailbox}
+  return Server{sock, hostNick, numClients, clients, mailbox}
 
 runHost :: Int -> IO ()
 runHost port = do
@@ -58,6 +55,7 @@ runHost port = do
   setSocketOption sock ReuseAddr 1
   bind sock (SockAddrInet (fromIntegral port) 0)
   listen sock Common.backlog
+  hSetBuffering stdout NoBuffering
   server <- newServer sock
   _ <- forkIO $ fix $ \loop ->
     hGetLine stdin >>= pure . Common.parseMessage >>= \case
@@ -69,7 +67,7 @@ runHost port = do
   _ <- forkIO $ fix $ clientListener server
   run server
  where
-  clientListener Server{sock, clients, clientMetadata, numClients, mailbox} loop = do
+  clientListener Server{sock, clients, numClients, mailbox} loop = do
     atomically $ modifyTVar numClients succ
     clientId <- atomically $ readTVar numClients
     putStrLn "Accepting a new client"
@@ -77,10 +75,9 @@ runHost port = do
     conn <- socketToHandle clientSock ReadWriteMode
     hSetBuffering conn NoBuffering
     putStrLn $ "Got client: " ++ (show addr)
-    let client' = Connection{clientId, conn}
+    let client' = Connection{clientId, conn, nick = Nothing}
     atomically $ modifyTVar clients (Map.insert clientId client')
-    atomically $ modifyTVar clientMetadata (Map.insert clientId Nothing)
-    _ <- forkIO $ handleClient mailbox Connection{clientId, conn}
+    _ <- forkIO $ handleClient mailbox client'
     loop
   run server@Server{mailbox} =
     (atomically $ tryReadTQueue mailbox) >>= \case
@@ -88,16 +85,19 @@ runHost port = do
       Nothing -> do
         run server
 
-broadcast :: Server -> ByteString -> IO ()
-broadcast Server{clients} message = do
+broadcast :: Server -> Maybe Int -> ByteString -> IO ()
+broadcast Server{clients} except message = do
   clients' <- (atomically $ readTVar clients)
-  forM_ (Map.elems clients') $ \Connection{conn} -> BS.hPutStrLn conn message
+  let withoutSender = Map.elems $ case except of
+        Just clientId -> Map.delete clientId clients'
+        Nothing -> clients'
+  forM_ withoutSender $ \Connection{conn} -> BS.hPutStrLn conn message
 
 handleMessage :: Server -> MessageSource -> IO ()
 handleMessage server@Server{hostNick, clients} (FromHost message) =
   case message of
     Quit -> do
-      broadcast server (BS.pack "Host closed room, byebye :P")
+      broadcast server Nothing (BS.pack "Host closed room, byebye :P")
       clients' <- Map.elems <$> (atomically $ readTVar clients)
       forM_ clients' $
         \Connection{conn} -> BS.hPutStrLn conn (BS.pack "Host closed room, byebye :P") *> hClose conn
@@ -105,41 +105,36 @@ handleMessage server@Server{hostNick, clients} (FromHost message) =
     ShowNick -> (atomically $ readTVar hostNick) >>= BS.putStrLn
     GotMessage msg -> do
       nick <- atomically $ readTVar hostNick
-      broadcast server (BS.concat [nick, BS.pack ": ", msg])
-handleMessage server@Server{clientMetadata, clients} (FromClient clientId message) =
+      broadcast server Nothing (BS.concat [nick, BS.pack ": ", msg])
+handleMessage server@Server{clients} (FromClient clientId message) =
   case message of
     Quit -> do
-      metadata <- (atomically $ readTVar clientMetadata)
-      msg <- case Map.lookup clientId metadata of
-        Just (Just nick) -> return $ BS.concat [nick, BS.pack " quit the server"]
-        Just Nothing -> return $ BS.pack $ "User " ++ (show clientId) ++ " quit the server"
-        Nothing -> failIO $ "Cannot find client " ++ (show clientId) ++ "'s nick"
+      clients' <- (atomically $ readTVar clients)
+      Connection{nick, conn} <- maybe (failIO $ "Client " ++ show clientId ++ " not found") pure $ Map.lookup clientId clients'
+      msg <- case nick of
+        Just n -> return $ BS.concat [n, BS.pack " quit the server"]
+        Nothing -> return $ BS.pack $ "User " ++ (show clientId) ++ " quit the server"
+      -- Close client conn and remove from client maps
+      hClose conn
       atomically $ modifyTVar clients (Map.delete clientId)
-      broadcast server msg
+      -- Broadcast to other users that client has left
+      broadcast server (Just clientId) msg
     SetNick newNick -> do
-      metadata <- (atomically $ readTVar clientMetadata)
-      msg <- case Map.lookup clientId metadata of
-        Just (Just oldNick) ->
-          return $ BS.concat [BS.pack "Client \'", oldNick, BS.pack "\' changed their nick to \'", newNick, BS.pack "\'"]
-        Just Nothing -> return $ BS.concat [BS.pack $ "User " ++ (show clientId) ++ " changed their nick to ", newNick]
-        Nothing -> failIO "Cannot find client nick"
-      atomically $ modifyTVar clientMetadata (Map.adjust (const $ Just newNick) clientId)
-      broadcast server msg
+      clients' <- (atomically $ readTVar clients)
+      msg <-
+        maybe
+          (return $ BS.concat [BS.pack $ "User " ++ (show clientId) ++ " changed their nick to ", newNick])
+          (\oldNick -> pure $ BS.concat [BS.pack "Client \'", oldNick, BS.pack "\' changed their nick to \'", newNick, BS.pack "\'"])
+          (Map.lookup clientId clients' >>= nick)
+      atomically $ modifyTVar clients (Map.adjust (\conn -> conn{nick = Just newNick}) clientId)
+      broadcast server (Just clientId) msg
     ShowNick -> do
-      metadata <- (atomically $ readTVar clientMetadata)
-      nick <- case Map.lookup clientId metadata of
-        Just (Just nick) -> return nick
-        Just Nothing -> return $ BS.pack $ "User " ++ show clientId
-        Nothing -> failIO "Cannot find client nick"
-      Map.lookup clientId <$> (atomically $ readTVar clients) >>= \case
-        Just Connection{conn} -> BS.hPutStrLn conn nick
-        Nothing -> failIO $ "Cannot find nick for client " ++ (show clientId)
+      clients' <- (atomically $ readTVar clients)
+      Connection{nick, conn} <- maybe (failIO $ "Client " ++ show clientId ++ " not found") pure $ Map.lookup clientId clients'
+      BS.hPutStrLn conn $ maybe (BS.pack $ "User " ++ show clientId) id nick
     GotMessage msg -> do
-      metadata <- (atomically $ readTVar clientMetadata)
-      withNick <- case Map.lookup clientId metadata of
-        Just (Just nick) -> return $ BS.concat [nick, BS.pack ": ", msg]
-        Just Nothing -> return msg
-        Nothing -> failIO "Cannot find client nick"
+      clients' <- (atomically $ readTVar clients)
+      withNick <- maybe (pure $ msg) (\nick -> pure $ BS.concat [nick, BS.pack ": ", msg]) (Map.lookup clientId clients' >>= nick)
       BS.putStrLn withNick
       hFlush stdout
       withoutSender <- Map.delete clientId <$> (atomically $ readTVar clients)
